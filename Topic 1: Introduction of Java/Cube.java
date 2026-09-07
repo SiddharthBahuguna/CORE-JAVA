5d38c664fb664f3c91aca64734efa4cb4915c7af
Siddharth Bahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2026-09-08 01:40:48 +0530

    Add files via upload

---
+++ Topic 1: Introduction of Java/Cube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Cube{
+ public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter side of cube:");
+  int n=sc.nextInt();
+  int ans=n*n*n;
+
+ System.out.print("Cube:"+ans);
+ }
+}
