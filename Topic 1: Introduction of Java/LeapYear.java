9378235c0e47095f60c9e905c0ec57bea14886e5
Siddharth Bahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2026-09-11 02:35:49 +0530

    Add files via upload

---
+++ Topic 1: Introduction of Java/LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LeapYear{
+ public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter year:");
+  int year = sc.nextInt();
+
+  if((year % 400==0) || (year % 4 ==0 && year % 100!=0)){
+  System.out.print("Leap Year");
+  }
+  else{
+  System.out.print("Not a Leap Year");
+  }
+ }
+}
