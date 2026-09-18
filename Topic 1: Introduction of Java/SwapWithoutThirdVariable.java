0067dc5c3e419986032586a1d68a998dd18e54c0
Siddharth Bahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2026-09-19 00:29:08 +0530

    Add files via upload

---
+++ Topic 1: Introduction of Java/SwapWithoutThirdVariable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SwapWithoutThirdVariable{
+ public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  
+  System.out.print("Enter A:");
+  int a = sc.nextInt();
+  
+  System.out.print("Enter B:");
+  int b = sc.nextInt();
+  
+  a = a+b;
+  b = a-b;
+  a = a-b;
+
+  System.out.println("A:"+a+ ",B:" +b);
+ }
+}
